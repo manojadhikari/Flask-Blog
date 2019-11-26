2d3f539beae51e7a11924609b79d04436c2cca3e
Manoj Adhikari <manojadhikari@MacBook-Pro-8.local>
2019-11-25 18:15:51 -0800

    Use templates to configure the html pages. Also initialize a css file

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)#Double underscore name is a special variable in python that is just the name of the module
+
+posts = [
+    {
+    'author': 'Manoj Adhikari',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2018'
+    },
+
+    {
+    'author': 'Jenna Rai',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2018'
+    }
+
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    name = request.args.get("name", "World")
-    return f'Home Page!'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    name = request.args.get("name", "World")
-    return f'About Page'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
